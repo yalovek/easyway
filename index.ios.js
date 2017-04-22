7328e1eac8d2860bb4fef64260d412ad20efb671
Vladimir Ivakhnenko <ivakhnenko@yamoney.ru>
2017-04-22 16:10:16 +0300

    charts

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -12,42 +6,17 @@ import {
   View
 } from 'react-native';
 
-export default class iqhack extends Component {
+import ChartsListScreen from './app/ChartsListScreen';
+
+class Example extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+
+      <ChartsListScreen>
+      </ChartsListScreen>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent('iqhack', () => Example);
 
-AppRegistry.registerComponent('iqhack', () => iqhack);
